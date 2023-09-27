9e8e6a447bd1366971adce5a6f3d81aa0da275fc
khadri2021 <khadri.institutions@gmail.com>
2023-09-27 08:59:46 +0530

    REST Archetype

---
+++ src/main/resources/archetype-resources/src/main/java/controller/TestRestController.java
@@ -0,0 +1,10 @@
+package com.khadri.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+
+}
